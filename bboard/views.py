aac514218804b6cc78ed3b78784b6b9068571b76
Igor Pentiya <igor.pentiya@gmail.com>
2021-12-10 14:53:59 +0300

    2.3

--- bboard/views.py
+++ bboard/views.py
@@ -5,7 +5,17 @@ from django.http import HttpResponse
 from django.template import loader
 
 from .models import Bb
+from .models import Rubric
 
 def index(request):
     bbs = Bb.objects.all()
-    return render( request, 'index.html', { 'bbs' : bbs })
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render( request, 'index.html', context)
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'by_rubric.html', context)
